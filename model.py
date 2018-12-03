a8aad24d2be891c7835de6d332e8542843ae934d
ericballouz <35756885+ericballouz@users.noreply.github.com>
2018-12-02 22:51:54 -0800

    Update model.py

--- model.py
+++ model.py
@@ -3,16 +3,20 @@ import check_if_on_track
 
 
 # input: state, and number of steps so far
-# output: reward of the state and whether the simulation continues
-# the simulation stops when we exit the world
+# output: reward of the state
 def R(s, N):
-    if check_if_on_track.check_if_car_in_world(s):
-        return -1000000, False
+    if not check_if_on_track.check_if_car_in_world(s):
+        return -1000000
 
     r = -0.1
     if check_if_on_track.check_if_car_on_track(s):
         r += 1000/N
-    return r, True
+    return r
+
+#def endGame(s, start, checkpoints):
+##    if not check_if_on_track.check_if_car_in_world(s):
+ #       return True
+ #   if
 
 # input: state, action, time step
 # output: next state
